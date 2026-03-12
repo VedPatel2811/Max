3ad518d144f3397c94c65c88e7e73bb4ffcf91c2
Ved Patel <veds28112004@gmail.com>
2026-03-11 20:22:33 -0400

    feat: Update Header and MobileSearch components with improved layout and search functionality

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -6,7 +6,7 @@ import UserAuth from "./UserAuth";
 export default function Header() {
   return (
     <header className="bg-[#000000] items-center border-b border-[#ffffff]/20 px-3">
-      <div className="flex md:grid md:grid-cols-3 justify-between items-center max-w-330 mx-auto py-3  md:px-6 ">
+      <div className="flex md:grid md:grid-cols-3 justify-between items-center max-w-330 mx-auto py-3 md:px-6 ">
         <MaxLogo />
 
         
